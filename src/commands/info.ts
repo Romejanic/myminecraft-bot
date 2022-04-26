0b25a6cbc019c506829162c3037c12cebad0e8b8
Jack Davenport <jack.davenport5011@gmail.com>
2022-04-26 13:30:28 +1000

    Updated bot invite link

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,7 +1,7 @@
 import { MessageEmbed } from "discord.js";
 import { Command } from "./manager";
 
-const INVITE_LINK = "";
+const INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=793150744533925888&permissions=3072&scope=bot%20applications.commands";
 const ICON_LINK   = "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/26/Block_of_Redstone_JE2_BE2.png/revision/latest/scale-to-width-down/300?cb=20191230030530";
 const EMBED_COLOR = "#4e7a39";
 
