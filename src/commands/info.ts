269e49f1d6833fb38dad8a3c5d1d1937c4f4ca65
Jack Davenport <jack.davenport5011@gmail.com>
2022-04-29 01:10:42 +1000

    Bot icon image is read from file

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,7 +1,7 @@
 import { MessageEmbed } from "discord.js";
 import { Command } from "./manager";
 
-import { EMBED_COLOR, ICON_LINK, INVITE_LINK, BUG_REPORTS, GITHUB_LINK, SUPPORT_SERVER } from '../const';
+import { EMBED_COLOR, INVITE_LINK, BUG_REPORTS, GITHUB_LINK, SUPPORT_SERVER, botIcon } from '../const';
 
 const InfoCommand: Command = async (ctx) => {
     // gather data for command
@@ -13,11 +13,12 @@ const InfoCommand: Command = async (ctx) => {
         `[Join the support server!](${SUPPORT_SERVER})`
     ];
     // create and send embed
+    const [iconName, icon] = botIcon();
     const embed = new MessageEmbed()
         .setTitle("MyMinecraft")
         .setColor(EMBED_COLOR)
         .setDescription("A clean and simple way to check the statuses of Minecraft servers.\n\n" + links.join("\n"))
-        .setThumbnail(ICON_LINK)
+        .setThumbnail(iconName)
         .setFooter({ text: "Created with ❤️ by @memedealer#6607" })
         .addField("Version", process.env.npm_package_version, true)
         .addField("Node Version", process.version, true)
@@ -25,7 +26,10 @@ const InfoCommand: Command = async (ctx) => {
         .addField("Memory Usage", (100 * mem.heapUsed / mem.heapTotal).toFixed(1) + "%", true)
         .addField("Serving", ctx.client.guilds.cache.size + " servers", true)
         .addField("Environment", process.env.NODE_ENV === "production" ? "Production" : "Development", true);
-    await ctx.reply(embed);
+    await ctx.reply({
+        embeds: [embed],
+        files: [icon]
+    });
 };
 
 export default InfoCommand;
