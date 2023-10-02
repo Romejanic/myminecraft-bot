af7c21c5ec02b879c933d7851756c083636bb0f7
Jack Davenport <jack.davenport5011@gmail.com>
2023-10-02 16:44:12 +1100

    Implement /info command

---
+++ src/const.ts
@@ -0,0 +1,15 @@
+import { AttachmentBuilder } from "discord.js";
+
+export const INVITE_LINK    = "https://discord.com/api/oauth2/authorize?client_id=793150744533925888&permissions=3072&scope=bot%20applications.commands";
+export const SUPPORT_SERVER = "https://discord.gg/fawJ2dTxFS";
+export const BUG_REPORTS    = "https://github.com/Romejanic/myminecraft-bot/issues/new/choose";
+export const GITHUB_LINK    = "https://github.com/Romejanic/myminecraft-bot";
+export const EMBED_COLOR    = "#4e7a39";
+
+export type EmbedImage    = [string, AttachmentBuilder];
+export type ServerAddress = [string, number];
+
+export function botIcon(): EmbedImage {
+    const name = "attachment://icon.png";
+    return [name, new AttachmentBuilder("icon.png")];
+}
